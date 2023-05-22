44b4c67290c3b90c7d17a43a305cbf6219603b21
enze.dai <enze.dai@chaitin.com>
2023-05-22 18:00:39 +0800

    feat(lexer): done.

--- lexer/lexer.cpp
+++ lexer/lexer.cpp
@@ -9,11 +9,66 @@ double NumVal;
 int getToken() {
     static int LastChar = ' ';
     
-    // whitespaces should not be in consideration
+    // Whitespaces should not be in consideration
     while (isspace(LastChar)) {
         LastChar = getchar();
     }
 
+    // Distinguish the identifier
+    if (isalpha(LastChar)) {
+        IdentifierStr = LastChar;
+
+        // Get next char until it's not a letter or number
+        while (isalnum(LastChar = getchar())) {
+            IdentifierStr += LastChar;
+        }
+
+        if (IdentifierStr == "def") {
+            return tok_def;
+        }
+
+        if (IdentifierStr == "extern") {
+            return tok_extern;
+        }
+
+        return tok_identifier;
+    }
+
+    // Distinguish the numerical variable
+    if (isdigit(LastChar) || LastChar == '.') {
+        std::string NumStr;
+
+        do {
+            NumStr += LastChar;
+            LastChar = getchar();
+        } while (isdigit(LastChar) || LastChar == '.');
+
+        // Convert string to double
+        NumVal = strtod(NumStr.c_str(), 0);
+        return tok_number;
+    }
+
+    // Cope with comments by skipping to the end of the line
+    if (LastChar == '#') {
+        do {
+            LastChar = getchar();
+        } while (LastChar != EOF && LastChar != '\n' && LastChar != '\r');
+
+        // Not the end of the file, call getToken() recursively
+        if (LastChar != EOF) {
+            return getToken();
+        }
+    }
+
+    // None of the above circumstances matched
+    if (LastChar == EOF) {
+        return tok_eof;
+    }
+
+    int ThisChar = LastChar;
+    LastChar = getchar();
+
+    return ThisChar;
 }
 
 int getNextToken() {
