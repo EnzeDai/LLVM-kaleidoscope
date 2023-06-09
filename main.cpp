07708db7831578236ce3a5faa6851774c0a6905c
enze.dai <enze.dai@chaitin.com>
2023-06-09 18:39:17 +0800

    feat: fill parser.h and almost done main.cpp.

---
+++ main.cpp
@@ -0,0 +1,81 @@
+// stdlib headers
+#include <algorithm>
+#include <cctype>
+#include <cstdlib>
+#include <cstdio>
+#include <map>
+#include <memory>
+#include <string>
+#include <vector>
+
+// LLVM headers
+#include "llvm/ADT/APFloat.h"
+#include "llvm/ADT/STLExtras.h"
+#include "llvm/IR/BasicBlock.h"
+#include "llvm/IR/Constants.h"
+#include "llvm/IR/DerivedTypes.h"
+#include "llvm/IR/Function.h"
+#include "llvm/IR/IRBuilder.h"
+#include "llvm/IR/LLVMContext.h"
+#include "llvm/IR/Module.h"
+#include "llvm/IR/Type.h"
+#include "llvm/IR/Verifier.h"
+
+// ast headers
+#include "ast/BinaryExprAST.h"
+#include "ast/CallExprAST.h"
+#include "ast/ExprAST.h"
+#include "ast/FunctionAST.h"
+#include "ast/NumberExprAST.h"
+#include "ast/PrototypeAST.h"
+#include "ast/VariableExprAST.h"
+
+// kaleidoscope header
+#include "kaleidoscope/kaleidoscope.h"
+
+// lexer headers
+#include "lexer/lexer.h"
+#include "lexer/token.h"
+
+// logger header
+#include "logger/logger.h"
+
+// parser header
+#include "parser/parser.h"
+
+using namespace llvm;
+
+// ==========================
+// ==  Handle Everything   ==
+// ==========================
+
+static void HandleDefinition() {
+
+}
+
+static void HandleExtern() {
+
+}
+
+static void HandleTopLevelExpression() {
+
+}
+
+static void MainLoop() {
+
+}
+
+int main() {
+    // Set operator priority
+    BinopPriority['<'] = 10;
+    BinopPriority['+'] = 20;
+    BinopPriority['-'] = 20;
+    BinopPriority['*'] = 40;
+
+    fprintf(stderr, "READY> ");
+    getNextToken();
+
+
+
+    return 0;
+}
--- parser/parser.h
+++ parser/parser.h
@@ -2,11 +2,26 @@
 #define __PARSER_H__
 
 #include <map>
-
+#include "ast/BinaryExprAST.h"
+#include "ast/CallExprAST.h"
+#include "ast/ExprAST.h"
+#include "ast/FunctionAST.h"
+#include "ast/NumberExprAST.h"
+#include "ast/PrototypeAST.h"
+#include "ast/VariableExprAST.h"
 #include "lexer/lexer.h"
 #include "lexer/token.h"
 
-extern std::map<char, int> BinopPrecedence;
-
+extern std::map<char, int> BinopPriority;
+std::unique_ptr<ExprAST> ParseNumberExpr();
+std::unique_ptr<ExprAST> ParseParentExpr();
+std::unique_ptr<ExprAST> ParseIdentifierExpr();
+std::unique_ptr<ExprAST> ParsePrimary();
+std::unique_ptr<ExprAST> ParseBinOpRHS(int ExprPrec, std::unique_ptr<ExprAST> LHS);
+std::unique_ptr<ExprAST> ParseExpression();
+std::unique_ptr<PrototypeAST> ParsePrototype();
+std::unique_ptr<FunctionAST> ParseDefinition();
+std::unique_ptr<FunctionAST> ParseTopLevelExpr();
+std::unique_ptr<PrototypeAST> ParseExtern();
 
 #endif
